7198cad98f5bca71ad73ae500ff86de8d6ca7a10
Kawamoto <kawamoto@kawamoto-THIRDWAVE.(none)>
2012-04-04 21:16:23 +0900

    use its method

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -6,11 +6,11 @@ describe MessageFilter do
   share_examples_for 'MessageFilter with argument "foo"' do
     it { should be_detect('hello from foo') }
     it { should_not be_detect('hello, world') }
+    its(:ng_words) { should_not be_empty }
   end
   context 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
     it_should_behave_like 'MessageFilter with argument "foo"'
-    it { subject.ng_words.should_not be_empty }
   end
   context 'with argument "foo","bar"' do
     subject { MessageFilter.new('foo', 'bar') }
