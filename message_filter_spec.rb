70dff5be50399031de602b394f8f9199e4bddd48
Kawamoto <kawamoto@kawamoto-THIRDWAVE.(none)>
2012-04-04 20:43:11 +0900

    clean code format

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -4,11 +4,6 @@ require './message_filter'
 
 describe MessageFilter, 'with argument "foo"' do
   subject { MessageFilter.new('foo') }
-  it {
-    should be_detect('hello from foo')
-  }
-  it {
-    should_not be_detect('hello, world!')
-  }
+  it { should be_detect('hello from foo') }
+  it { should_not be_detect('hello, world!') }
 end
-
