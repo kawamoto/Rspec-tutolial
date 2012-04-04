ea108da61817a4106c82161b17b0b7b3de7d7c31
Kawamoto <kawamoto@kawamoto-THIRDWAVE.(none)>
2012-04-04 20:26:02 +0900

    1st

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,6 +1,15 @@
 require 'rubygems'
 require 'rspec'
+require './message_filter'
 
 describe MessageFilter do
+  it 'should detect message with NG word' do
+    filter = MessageFilter.new('foo')
+    filter.detect?('hello from foo').should == true
+  end
+  it 'should not detect message without NG word' do
+    filter = MessageFilter.new('foo')
+    filter.detect?('hello, world!').should == false
+  end
 end
 
