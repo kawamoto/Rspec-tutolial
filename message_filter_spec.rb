f780dd8d1f8e63f682b537da2b385fca18795351
Kawamoto <kawamoto@kawamoto-THIRDWAVE.(none)>
2012-04-04 21:01:36 +0900

    nest describe

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,17 +2,18 @@ require 'rubygems'
 require 'rspec'
 require './message_filter'
 
-share_examples_for 'MessageFilter with argument "foo"' do
-  it { should be_detect('hello from foo') }
-  it { should_not be_detect('hello, world') }
-end
-
-describe MessageFilter, 'with argument "foo"' do
-  subject { MessageFilter.new('foo') }
-  it_should_behave_like 'MessageFilter with argument "foo"'
-end
-describe MessageFilter, 'with argument "foo","bar"' do
-  subject { MessageFilter.new('foo', 'bar') }
-  it { should be_detect('hello from bar') }
-  it_should_behave_like 'MessageFilter with argument "foo"'
+describe MessageFilter do
+  share_examples_for 'MessageFilter with argument "foo"' do
+    it { should be_detect('hello from foo') }
+    it { should_not be_detect('hello, world') }
+  end
+  describe 'with argument "foo"' do
+    subject { MessageFilter.new('foo') }
+    it_should_behave_like 'MessageFilter with argument "foo"'
+  end
+  describe 'with argument "foo","bar"' do
+    subject { MessageFilter.new('foo', 'bar') }
+    it { should be_detect('hello from bar') }
+    it_should_behave_like 'MessageFilter with argument "foo"'
+  end
 end
