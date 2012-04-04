77d3aa0a119126e991a46248a34d73b3c03f2f03
Kawamoto <kawamoto@kawamoto-THIRDWAVE.(none)>
2012-04-04 20:57:53 +0900

    use share_examples_for method

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,14 +2,17 @@ require 'rubygems'
 require 'rspec'
 require './message_filter'
 
+share_examples_for 'MessageFilter with argument "foo"' do
+  it { should be_detect('hello from foo') }
+  it { should_not be_detect('hello, world') }
+end
+
 describe MessageFilter, 'with argument "foo"' do
   subject { MessageFilter.new('foo') }
-  it { should be_detect('hello from foo') }
-  it { should_not be_detect('hello, world!') }
+  it_should_behave_like 'MessageFilter with argument "foo"'
 end
 describe MessageFilter, 'with argument "foo","bar"' do
   subject { MessageFilter.new('foo', 'bar') }
   it { should be_detect('hello from bar') }
-  it { should be_detect('hello from foo') }
-  it { should_not be_detect('hello, world!') }
+  it_should_behave_like 'MessageFilter with argument "foo"'
 end
